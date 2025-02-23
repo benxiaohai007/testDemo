02b7faf852331a8539e5e4c19a0b9968003d45a4
benxioahai <13753146554@163.com>
2025-02-22 19:51:39 -0800

    test

--- config/config.py
+++ config/config.py
@@ -2,7 +2,7 @@
 import yaml
 from pathlib import Path
 
-def load_config(config_path: str = 'config/config.yaml'):
+def load_config(config_path: str = 'config.yaml'):
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
 
@@ -10,4 +10,13 @@ class Config:
     def __init__(self, config_dict):
         self.__dict__.update(config_dict)
 
+    def __str__(self):
+        return str(self.__dict__)
 config = Config(load_config())
+
+
+if __name__ == '__main__':
+    config = Config(load_config())
+    print(config)
+    print(type(config))
+    print(config.environments.get('test'))
